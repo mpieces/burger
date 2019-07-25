52678bbf657bc465c75eb72ef69514c78b4ef4da
Marisa Ramon <mer0522@gmail.com>
2019-07-24 19:29:43 -0500

    delete unused routes from server.js

--- server.js
+++ server.js
@@ -23,65 +23,7 @@ var routes = require ('./controller/burger_controller.js')
 app.use (routes);
 
 
-// Serve index.handlebars to the root route.
-// app.get("/", function(req, res) {
-//   connection.query("SELECT * FROM burgers;", function(err, data) {
-//     if (err) {
-//       return res.status(500).end();
-//     }
 
-//     res.render("index", { burgers: data });
-//   });
-// });
-
-// Show the user the individual quote and the form to update the quote.
-app.get("/:id", function(req, res) {
-  connection.query("SELECT * FROM burgers where id = ?", [req.params.id], function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    console.log(data);
-    res.render("burger", data[0]);
-  });
-});
-
-app.post("/api/burgers", function(req, res) {
-  connection.query("INSERT INTO burgers VALUES (?, ?)", [req.body.burger, req.body.burger], function(
-    err,
-    result
-  ) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
-
-
-
-// // Update a quote by an id and then redirect to the root route.
-app.put("/api/burgers/:id", function(req, res) {
-  connection.query(
-    "UPDATE burgers SET burger_name = ?, devoured = ? WHERE id = ?",
-    [req.body.burger_name, req.body.devoured, req.params.id],
-    function(err, result) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      }
-      else if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-
-    }
-  );
-});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
